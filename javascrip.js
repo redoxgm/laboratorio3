b44bc81d06b74103f1df8895c52776fb1e0ddcc1
German Maldonado <germanmaldonado962@gmail.com>
2023-04-24 19:35:59 -0300

    se agrego seccion comentarios a index

--- javascrip.js
+++ javascrip.js
@@ -8,40 +8,29 @@ function iniciar(){
 }
 
 function btnPublicar1(){
-    let resena = document.getElementById("resenas")
-    let nombreUsuario = document.getElementById("txtNombre")
+   
+    let nombreUsuario = document.getElementById("txtNombre");
     let txtNombre = nombreUsuario.value;
-    let emailUsuario = document.getElementById("txtEmail")
+    let emailUsuario = document.getElementById("txtEmail");
     let txtEmail = emailUsuario.value;
-    let Resena = document.getElementById("txtResena")
+    let Resena = document.getElementById("txtResena");
     let txtresena = Resena.value;
 
-    let nombre = document.createElement("p");
-    let resena1=  document.createTextNode(txtresena);
-    let email= document.createAttribute("p")
-
-    var elementResena = document.getElementById("resenas");
-    elementResena.appendChild(nombre,email,resena1)
-
-    // array.push(txtNombre);
-    // array.push(txtEmail);
-    // array.push(txtresena);
-    // contruirResena();
+    let opcionNumerica = document.getElementById("selecValoracion").value;
     
-}
 
-// function contruirResena(){
 
-    
-//     let resenaContruida= document.getElementById("resenas")
-//     let resena=""
-//     for(let tarea of array ){
-//        resena += tarea + "<br/>";
-//     }
-
-    
-//     resenaContruida.innerHTML=resena;
-    
 
    
-// }
+    let resenaDiv = document.createTextNode(txtresena + ' - ' + opcionNumerica);
+    
+    let parrafo = document.createElement('p');
+   
+    parrafo.appendChild(resenaDiv );
+   
+    document.getElementById("resenas").appendChild(parrafo);
+  
+
+ 
+
+}
