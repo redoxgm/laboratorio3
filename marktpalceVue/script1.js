eb2926130549d5b843bcdd057859f5bb3130558a
German Maldonado <germanmaldonado962@gamil.com>
2023-05-24 21:23:23 -0300

    se realizaron los puntos obligatorios en producto

--- marktpalceVue/script1.js
+++ marktpalceVue/script1.js
@@ -1,6 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
+            carro: 0,
+            selectRam:0,
             "id": 1,
             "title": "Notebook HP 14-dq2024la",
             "description": "Computadora HP orientado para gama media. Procesador Intel® Core™ i3 de 11.ª generación.  Windows 10 Home 64.  Unidad de estado sólido PCIe® NVMe™ M.2 de 256 GB . Pantalla de 14 pulgadas.",
@@ -8,16 +10,39 @@ const app = Vue.createApp({
             "factory_url": "https://www.hp.com/ar-es/shop/notebook-hp-14-dq2024la-3v8j6la.html",
             "notebooksTypes": [
                 {
+                    id:0,
                     "ramAmount": "8 GB",
                     "price": 100000
                 },
                 {
+                    id:1,
                     "ramAmount": "16 GB",
                     "price": 122547
                 }
             ]
         }
-    }
+        
+    },
+    methods: {
+        agregar(){
+
+            return this.carro = this.carro + 1
+        },
+
+        
+        precio(indice){
+
+            return this.notebooksTypes[indice].ramAmount
+            
+        }
+
+
+    },
+
+   
+    
+    
+
 })
 
 app.mount('#grid-layout')
