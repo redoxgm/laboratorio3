21b50ff902a5f5a043d9b2b44260065279cc56a6
German Maldonado <germanmaldonado962@gmail.com>
2023-05-26 12:43:55 -0300

    barra imagnes procductos isn funcionar

--- marktpalceVue/script1.js
+++ marktpalceVue/script1.js
@@ -8,12 +8,14 @@ const app = Vue.createApp({
             precio:0,
             
             imagenesNotebook:[
-
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS6RwLPY5R9R3uKnz0Nmy5tFf17DM61WKXhlBjCbfWMl6osiVzl-LIOy3WHltd66j9WXNU&usqp=CAU',
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJQSNOWkS20mQugebEQa-b0ZTfN-oWKro9cR2gZ72zkpFeewAqy7kGEXsYcQqKy9u6Olw&usqp=CAU',
+                "https://ar-media.hptiendaenlinea.com/catalog/product/8/V/8VW01LA-1_T1615590539.png"
             ],
             "id": 1,
             "title": "Notebook HP 14-dq2024la",
-            "description": "Computadora HP orientado para gama media. Procesador Intel® Core™ i3 de 11.ª generación.  Windows 10 Home 64.  Unidad de estado sólido PCIe® NVMe™ M.2 de 256 GB . Pantalla de 14 pulgadas.",
             "image_url": "https://ar-media.hptiendaenlinea.com/catalog/product/8/V/8VW01LA-1_T1615590539.png",
+            "description": "Computadora HP orientado para gama media. Procesador Intel® Core™ i3 de 11.ª generación.  Windows 10 Home 64.  Unidad de estado sólido PCIe® NVMe™ M.2 de 256 GB . Pantalla de 14 pulgadas.",
             "factory_url": "https://www.hp.com/ar-es/shop/notebook-hp-14-dq2024la-3v8j6la.html",
             "notebooksTypes": [
                 {
@@ -56,6 +58,9 @@ const app = Vue.createApp({
             return  this.carro.push(this.notebooksTypes[this.selectRam])
         },
        
+        cambiarImagen(imagenUrl) {
+            this.image_url = imagenUrl;
+        },
       
     },
    
