eeb37d617f4a876dc5cafed11140dbb8093e10f8
German Maldonado <germanmaldonado962@gmail.com>
2023-05-26 12:27:38 -0300

    se meodifico metodo agregar,el lisatdo de compra y el carrito

--- marktpalceVue/script1.js
+++ marktpalceVue/script1.js
@@ -1,8 +1,15 @@
 const app = Vue.createApp({
     data() {
         return {
-            carro: 0,
+            carro: [],
+            a:0,
             selectRam:0,
+            listaNotebookSeleccionada:[],
+            precio:0,
+            
+            imagenesNotebook:[
+
+            ],
             "id": 1,
             "title": "Notebook HP 14-dq2024la",
             "description": "Computadora HP orientado para gama media. Procesador Intel® Core™ i3 de 11.ª generación.  Windows 10 Home 64.  Unidad de estado sólido PCIe® NVMe™ M.2 de 256 GB . Pantalla de 14 pulgadas.",
@@ -24,36 +31,57 @@ const app = Vue.createApp({
         
     },
     methods: {
-        agregar(){
-
-            return this.carro = this.carro + 1
-        },
+        
 
         
-        ram(indice){
+
+        ram(indice) {
 
             return this.notebooksTypes[indice].ramAmount
-            
+
         },
 
-        PrecioSeleccionado(indice){
+        PrecioSeleccionado(indice) {
 
             this.selectRam = indice
-           
+            this.listaNotebookSeleccionada.push(this.notebooksTypes[indice])
+                       
+
         },
 
+
+        agregar() {
+
+           
+            return  this.carro.push(this.notebooksTypes[this.selectRam])
+        },
+       
       
-
-
     },
+   
     computed:{
 
-        mostrarPrecioSeleccionado(){
 
-            return  this.notebooksTypes[this.selectRam].price
+        mostrarPrecioeleccionado(){
+            return this.precio = this.notebooksTypes[this.selectRam].price
+        },
+
+
+        sumaPreciosSeleccionados(){
+
+            let suma=0;
+
+            for(let item of this.carro){
+
+               suma += item.price;
+                
+            }
+
+            return suma;
 
         }
 
+        
 
     }
 
